0dd873e4acb7a06e5c8a09c42c7ee5ee15464c20
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-06 10:57:37 +0200

    fix k3s install to use kubectl correctly

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
+# apt-get update
+#
+# echo "Installing k3s on server."
+# curl -sfL https://get.k3s.io | sh -
+# cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
+# chmod 644 /vagrant/node-token
+#
+# echo "Installing Kubectl on server."
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode 644" sh -
+# # curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+# # curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" sh -
+# # chmod +x ./kubectl
+# # mv kubectl /usr/local/bin/
+# # # Avoids needing to be sudo
+# # mkdir -p /home/vagrant/.kube
+# # cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+# # chown -R vagrant:vagrant /home/vagrant/.kube
+#
+# echo "Provisionning is finished."
+
+#!/bin/bash
 apt-get update
 
 echo "Installing k3s on server."
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode 644" sh -
+
 cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
 chmod 644 /vagrant/node-token
-
-echo "Installing Kubectl on server."
-# curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" sh -
-chmod +x ./kubectl
-mv kubectl /usr/local/bin/
-# Avoids needing to be sudo
-mkdir -p /home/vagrant/.kube
-cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-chown -R vagrant:vagrant /home/vagrant/.kube
 
 echo "Provisionning is finished."
