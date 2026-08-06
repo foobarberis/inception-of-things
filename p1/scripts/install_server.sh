94c4d3c49aedaede43c30d1f06183524d9e44577
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-06 10:47:46 +0200

    fix: internal ip of VMs

--- p1/scripts/install_server.sh
+++ p1/scripts/install_server.sh
@@ -7,7 +7,8 @@ cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
 chmod 644 /vagrant/node-token
 
 echo "Installing Kubectl on server."
-curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+# curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" sh -
 chmod +x ./kubectl
 mv kubectl /usr/local/bin/
 # Avoids needing to be sudo
--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -8,5 +8,6 @@ while [ ! -f /vagrant/node-token ]; do
 done
 
 echo "Installing k3s on worker."
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/node-token) sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/node-token) INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=eth1" sh -
+# curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/node-token) sh -
 echo "Provisionning is finished."
