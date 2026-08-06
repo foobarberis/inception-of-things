65d57518881b77bc8daea25c17b1d3e239939168
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-06 10:39:31 +0200

    fix k3s server

--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -8,5 +8,5 @@ while [ ! -f /vagrant/node-token ]; do
 done
 
 echo "Installing k3s on worker."
-K3S_URL=https://192.168.56.110:6443 TOKEN=$(cat /vagrant/node-token) curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/node-token) sh -
 echo "Provisionning is finished."
