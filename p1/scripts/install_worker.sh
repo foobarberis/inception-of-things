e1d4f8d9f27b62f9a7b4a709d3a4734af5ece044
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-12 10:57:00 +0200

    fix ip adress

--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -6,7 +6,9 @@ echo "Waiting for token..."
 while [ ! -f /vagrant/node-token ]; do
   sleep 2
 done
+NODE_INTERFACE=$(ip -o -4 addr show to 192.168.56.111 | awk '{print $2}')
+    echo "Cluster interface : $NODE_INTERFACE"
 
 echo "Installing k3s on worker."
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/node-token) INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=ens3" sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/node-token) INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=${NODE_INTERFACE}" sh -
 echo "Provisionning is finished."
