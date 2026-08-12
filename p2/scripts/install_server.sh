9fe6d570347e4192e392389899b538166d091332
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-12 12:35:23 +0200

    testing...

--- p2/scripts/install_server.sh
+++ p2/scripts/install_server.sh
@@ -6,6 +6,11 @@ NODE_INTERFACE=$(ip -o -4 addr show to 192.168.56.110 | awk '{print $2}')
 echo "Installing k3s on server."
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=${NODE_INTERFACE} --write-kubeconfig-mode 644" sh -
 
+echo "Waiting for k3s to start and generate node-token..."
+while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
+  sleep 2
+done
+
 cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
 chmod 644 /vagrant/node-token
 
