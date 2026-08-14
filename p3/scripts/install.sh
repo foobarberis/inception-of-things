bd70e158147184d2cc37b1f35820a204f67fa1fd
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-14 09:28:29 +0200

    fix docker install

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 set -e
 
+# Add Docker's official GPG key:
+sudo apt update -qq
+sudo apt install -qq ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update -qq
+sudo apt install -y -qq \
+        docker-ce docker-ce-cli containerd.io \
+        docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker "$USER"
+
 curl -fsSL https://get.docker.com | sh
 
 KUBECTL_VERSION=$(curl -fsSL https://dl.k8s.io/release/stable.txt)
