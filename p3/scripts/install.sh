1c12eb553a34ad67270dba836e74d94645acce21
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-14 15:16:30 +0200

    test portforwarding without special command

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -13,7 +13,10 @@ if ! command -v k3d &>/dev/null; then
 fi
 
 if ! k3d cluster list | grep -q "iot-p3"; then
-    k3d cluster create iot-p3
+    k3d cluster create iot-p3 \
+    -p "8888:8888@loadbalancer" \
+    -p "8086:80@loadbalancer" \
+    --wait
 fi
 
 export KUBECONFIG="$HOME/.kube/config"
@@ -27,17 +30,16 @@ kubectl apply -n argocd \
 kubectl wait --for=condition=available \
     --timeout=300s deployment/argocd-server -n argocd
 
+kubectl patch configmap argocd-cmd-params-cm -n argocd \
+    --type merge -p '{"data": {"server.insecure": "true"}}'
+kubectl rollout restart deployment argocd-server -n argocd
+kubectl rollout status deployment argocd-server -n argocd
+
 kubectl apply -f "$(dirname "$0")/../confs/argocd.yaml"
 
 echo "Finished setup"
-
-echo "To Port-forward appli to the host, do :"
-echo "  Inside the VM : kubectl port-forward svc/wil-playground 8888:8888 -n dev --address 0.0.0.0 &"
-echo "  On the host : curl http://localhost:8888/"
-echo ""
+echo "App    : curl http://localhost:8888/"
+echo "ArgoCD : http://localhost:8086  (login: admin)"
 echo "To get the password, do :"
 echo "  kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d"
 echo ""
-echo "To Port-forward Argo CD to the host, do :"
-echo "  Inside the VM : kubectl port-forward svc/argocd-server 8086:443 -n argocd --address 0.0.0.0 &"
-echo "  On the host : https://localhost:8086  (login: admin)"
