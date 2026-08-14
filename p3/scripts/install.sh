d0414568f01e451de271e6f9808762a63bcc2373
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-14 11:22:49 +0200

    change port

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -39,5 +39,5 @@ echo "Mot de passe Argo CD :"
 kubectl get secret argocd-initial-admin-secret -n argocd \
     -o jsonpath='{.data.password}' | base64 -d
 echo "UI Argo CD :"
-echo "  kubectl port-forward svc/argocd-server 8080:443 -n argocd &"
-echo "  https://localhost:8080  (login: admin)"
+echo "  kubectl port-forward svc/argocd-server 8086:443 -n argocd &"
+echo "  https://localhost:8086  (login: admin)"
