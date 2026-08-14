d6b5a0617152f2595f81d3acbcfa76923dbd518d
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-14 11:26:06 +0200

    added line

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -38,6 +38,7 @@ echo ""
 echo "Mot de passe Argo CD :"
 kubectl get secret argocd-initial-admin-secret -n argocd \
     -o jsonpath='{.data.password}' | base64 -d
+echo ""
 echo "UI Argo CD :"
 echo "  kubectl port-forward svc/argocd-server 8086:443 -n argocd &"
 echo "  https://localhost:8086  (login: admin)"
