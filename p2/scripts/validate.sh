cd162c4b556d9c341f63a78d9f57b93bc9c6ccfa
Matthieu Barberis <foobarberis@gmail.com>
2026-08-18 16:39:26 +0200

    Add validation script for P2

---
+++ p2/scripts/validate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+vagrant status
+vagrant ssh mbernardS2 -c 'hostname; ip -o -4 addr show to 192.168.56.110'
+vagrant ssh mbernardS2 -c 'kubectl get nodes -o wide'
+vagrant ssh mbernardS2 -c 'kubectl get all'
+vagrant ssh mbernardS2 -c 'kubectl get deployment app1 app2 app3; kubectl get pods -o wide'
+vagrant ssh mbernardS2 -c 'kubectl get ingress -o wide; kubectl describe ingress apps-ingress'
+
+curl -fsS -H 'Host: app1.com' http://192.168.56.110/ | grep -F 'Hello from app1.'
+curl -fsS -H 'Host: app2.com' http://192.168.56.110/ | grep -F 'Hello from app2.'
+curl -fsS http://192.168.56.110/ | grep -F 'Hello from app3.'
