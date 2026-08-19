310d7d5ebc31dafac67e0d6f117a75c16af24cc1
Mélodie BERNARD <mbernard@student.42lyon.fr>
2026-08-19 13:32:06 +0200

    no longer https : port 80

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -15,7 +15,7 @@ fi
 if ! k3d cluster list | grep -q "iot-p3"; then
     k3d cluster create iot-p3 \
     -p "8888:8888@loadbalancer" \
-    -p "8086:443@loadbalancer" \
+    -p "8086:80@loadbalancer" \
     --wait
 fi
 
@@ -44,7 +44,7 @@ kubectl apply -f "$(dirname "$0")/../confs/argocd.yaml"
 
 echo "Finished setup"
 echo "App    : curl http://localhost:8888/"
-echo "ArgoCD : https://localhost:8086  (login: admin)"
+echo "ArgoCD : http://localhost:8086  (login: admin)"
 echo "To get the password, do :"
 echo "  kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d"
 echo ""
